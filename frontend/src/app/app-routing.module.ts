7f6460018c71cc586ddd6c96de4b578ead070f44
Amalesh Arivanan <amalesh.unofficial@gmail.com>
2025-02-26 13:51:48 -0800

    path redirected to /users

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersListComponent } from './users-list/users-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'users', component: UsersListComponent},
+  {path: '', redirectTo: '/users', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
